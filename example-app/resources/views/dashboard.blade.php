fc85b41dd74fbbe3937b804b946f291942e3f30e
windsonms <phersona.digital@gmail.com>
2022-09-12 10:44:20 -0300

    first commit

---
+++ example-app/resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+  
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+  
+                <div class="card-body">
+                    @if (session('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+  
+                    You are Logged In
+                    <body>
+    
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="/products">Products</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
